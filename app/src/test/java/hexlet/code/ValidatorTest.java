af395f2a4c979cb4f9b76817ddea288197bd4028
KryWeak <lohovic6@gmail.com>
2025-08-20 03:08:55 +0300

    fixed the errors found by tests

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -8,7 +8,8 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class ValidatorTest {
 
