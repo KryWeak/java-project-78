8cf4477553f6ded16667564da99b1efce4a9f1af
KryWeak <lohovic6@gmail.com>
2025-09-18 19:01:21 +0300

    Update StringSchema

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,19 +1,38 @@
 package hexlet.code.schemas;
 
+import java.util.Objects;
+
 public final class StringSchema extends BaseSchema<String> {
 
+    private boolean required = false;
+
     public StringSchema required() {
-        addCheck(value -> value != null && !value.isEmpty());
+        required = true;
         return this;
     }
 
     public StringSchema minLength(int length) {
-        addCheck(value -> value == null || value.length() >= length);
+        addCheck(v -> v == null || v.length() >= length);
         return this;
     }
 
     public StringSchema contains(String substring) {
-        addCheck(value -> value == null || value.contains(substring));
+        addCheck(v -> v == null || v.contains(substring));
         return this;
+    }
+
+    @Override
+    public boolean isValid(Object value) {
+        if (value == null) {
+            return !required;
+        }
+        if (!(value instanceof String)) {
+            return false;
+        }
+        String str = (String) value;
+        if (required && str.isEmpty()) {
+            return false;
+        }
+        return super.isValid(str);
     }
 }
